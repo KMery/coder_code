b65bb1235a265e4c40359d8d00a69090f0466716
KMery <kmeryleiva@gmail.com>
2021-07-17 18:56:40 -0300

    se agregan validaciones de campos requeridos en POST

--- desafio9/src/middleware/checkAttr.js
+++ desafio9/src/middleware/checkAttr.js
@@ -1,7 +1,51 @@
-const product_data = ['id', 'title', 'price', 'thumbnail'];
-//Verifica los atributos recibidos sean correctos
+//Control de atributos mandados
+const product_data = ['title', 'price', 'thumbnail'];
+
+//Validación de atributos que envia el cliente
 const checkAttr = (req, res, next) => {
+    let required_data = requiredData(req);
+    let wrong_data = checkWrongData(req);
+
+    let msg = [];
+    if (required_data !== undefined) {
+        msg.push(required_data);
+    };
+
+    if (wrong_data !== undefined) {
+        msg.push(wrong_data);
+    };
+
+    if (msg.length > 0) {
+        return res.status(400).send(msg);
+    };
+
+    next();
+}
+
+//Valida si están los campos requeridos
+const requiredData = (req) => {
+    let required_data = [];
+
+    product_data.forEach(attr => {
+        if (req.body[attr] === undefined) {
+            required_data.push(attr)
+        }
+    });
+
+    if (required_data.length > 0) {
+        let msg = {
+            'error': {
+                'required fields': required_data
+            }
+        };
+        return msg;
+    };
+}
+
+//Valida si existe data erronea
+const checkWrongData = (req) => {
     let wrong_data = [];
+
     Object.keys(req.body).forEach(attribute => {
         if (product_data.includes(attribute) === false) {
             wrong_data.push(attribute);
@@ -10,12 +54,12 @@ const checkAttr = (req, res, next) => {
 
     if (wrong_data.length > 0) {
         let msg = {
-            'error': `The following data provided is wrong: ${wrong_data}`
+            'error': {
+                'wrong data': wrong_data
+            }
         };
-        return res.send(msg);
+        return msg;
     };
-
-    next();
-}
+};
 
 module.exports = checkAttr;
